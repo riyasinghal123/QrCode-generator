afa4849d06aa7d6ec72f2ced24ade12474dc8637
Riya <96625454+riyasinghal123@users.noreply.github.com>
2024-07-17 20:08:23 +0530

    Add files via upload

---
+++ qr1.py
@@ -0,0 +1,13 @@
+import qrcode
+from PIL import Image
+
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_H,
+    box_size=10,
+    border=4,
+)
+qr.add_data("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl&index=1")
+qr.make(fit=True)
+img=qr.make_image(fill_color="black",back_color="white")
+img.save("youtube_web.png")
